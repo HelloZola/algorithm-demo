8318516ea5d0a978fca85b0e09bbcc3407ae4783
HelloZola <643074224@qq.com>
2020-10-25 00:55:42 +0800

    分支提交

---
+++ src/test/java/comon/CommonTest.java
@@ -0,0 +1,20 @@
+package comon;
+
+public class CommonTest {
+
+    public static void main(String[] args) {
+
+        String str = "hello0";
+        String str2 = str;
+        str2 = "hello1";
+
+        String arr[] = new String[10];
+        String arr2[] = new String[10];
+
+        System.out.println(str.indexOf("ll"));
+
+        System.out.println(str.length());
+    }
+
+
+}
